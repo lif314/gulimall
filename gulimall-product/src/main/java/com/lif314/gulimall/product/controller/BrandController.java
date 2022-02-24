91d8ce19c1cfde98efd33d5162e37b00f541fc37
lif314 <2216155077@qq.com>
2022-02-24 22:03:11 +0800

    品牌管理-数据校验

--- gulimall-product/src/main/java/com/lif314/gulimall/product/controller/BrandController.java
+++ gulimall-product/src/main/java/com/lif314/gulimall/product/controller/BrandController.java
@@ -1,10 +1,12 @@
 package com.lif314.gulimall.product.controller;
 
 import java.util.Arrays;
+import java.util.HashMap;
 import java.util.Map;
 
 //import org.apache.shiro.authz.annotation.RequiresPermissions;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -16,6 +18,7 @@ import com.lif314.gulimall.product.service.BrandService;
 import com.lif314.common.utils.PageUtils;
 import com.lif314.common.utils.R;
 
+import javax.validation.Valid;
 
 
 /**
@@ -56,13 +59,29 @@ public class BrandController {
 
     /**
      * 保存
+     * @param brand 请求体 Post
+     * @param validateResult 校验结果
+     * @return 统一消息提示
      */
     @RequestMapping("/save")
    // @RequiresPermissions("product:brand:save")
-    public R save(@RequestBody BrandEntity brand){
-		brandService.save(brand);
-
-        return R.ok();
+    public R save(@Valid @RequestBody BrandEntity brand, BindingResult validateResult){
+		if(validateResult.hasErrors()) {
+            Map<String, String> map = new HashMap<>();
+            // 获取校验的错误结果
+            validateResult.getFieldErrors().forEach((item) -> {
+                // 获取所有的错误结果
+                // 获取@NotBlank中写的message
+                String message = item.getDefaultMessage();
+                // 获取错误属性的名字
+                String field = item.getField();
+                map.put(field, message);
+            });
+            return R.error(400, "提交数据不合法").put("data", map);
+        }else{
+            brandService.save(brand);
+            return R.ok();
+        }
     }
 
     /**
